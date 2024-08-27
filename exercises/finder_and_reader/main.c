5e4fa8f619e740bc818d58cda1837d56e311f1ca
Kjetil Indrehus <kjetilindrehus@gmail.com>
2024-08-27 13:50:51 +0200

    add: exercise get line method to read line by line

--- exercises/finder_and_reader/main.c
+++ exercises/finder_and_reader/main.c
@@ -1,3 +1,4 @@
+#define _GNU_SOURCE
 #include <pthread.h>  
 #include <stdlib.h>
 #include <stdio.h> 
@@ -20,12 +21,16 @@ FILE* file;
 pthread_t thread_id[2]; 
 
 void* reader(void* arg){
-    char line[1024];  // Allocate memory for line 
+    char* line = NULL; 
+    size_t len = 0;
+    ssize_t read;
 
     printf("Reader thread started\n");
-    while (fgets(line, sizeof(line), file) != NULL){
+    while ((read = getline(&line, &len, file)) != -1){
         // Acquire lock
         pthread_mutex_lock(&lock);
+
+        printf("Line from reader: %s", line);
 
         // CS: Read to buffer 
         if (buffer[0] == '\0'){  // Correct comparison
@@ -38,8 +43,6 @@ void* reader(void* arg){
         usleep(100);  // To avoid tight loop
     }
 
-
-
     // Buffer is empty. Acquire lock to tell end of file (EOF)
     pthread_mutex_lock(&lock);
     buffer[0] = EOF; 
@@ -47,11 +50,14 @@ void* reader(void* arg){
 
     printf("Reader thread stopped\n");
 
+    // Free the line pointer 
+    free(line);
+
     return NULL;
 }
 
 void* finder(void* arg){
-    char line[1024];
+    char* line = NULL; 
 
     printf("Finder thread started\n");
 
@@ -61,7 +67,7 @@ void* finder(void* arg){
 
         // CS:  Get from buffer and remove data from it 
         if (buffer[0] != '\0' && buffer[0] != EOF) {
-            line[0] = strdup(buffer);  // Duplicate buffer into line
+            line = strdup(buffer); 
             buffer[0] = '\0';  // Clear the buffer
         }
 
@@ -72,8 +78,6 @@ void* finder(void* arg){
 
         // Release lock 
         pthread_mutex_unlock(&lock);
-
-        printf("Line: %s \n", line);
 
         // Compare and check for the pattern 
         if (strstr(line, pattern) != NULL) {
@@ -120,6 +124,10 @@ int main(){
     pthread_join(thread_id[0], NULL); 
     pthread_join(thread_id[1], NULL); 
     pthread_mutex_destroy(&lock); 
+
+
+    // Mark file pointer to throw 
+    fclose(file);
   
     return 0; 
 }
